b2302a0a36aca3d626019409c7ad52fab37bd26e
Martin Matějek <matejm14@fit.cvut.cz>
2016-10-11 17:41:00 +0200

    Rewrite code to class for easier passing of variables

--- gh-issue-bot.py
+++ gh-issue-bot.py
@@ -7,65 +7,62 @@ import click
 import json
 import time
 
-def init_session (token):
-    session = requests.Session()
-    session.headers = {'Authorization': 'token ' + token, 'User-Agent': 'Python'}
-
-    return session
-
-def request (session, req):
-    print("Trying request: ", req)
-    r = session.get(req)
-
-    print("JSON response: ", json.dumps(r.json(), sort_keys=True, indent=4)) # pretty print
-    print("Response status code: ", r.status_code)
-
-    return r.json()
-
-@click.command()
-@click.option('--repo', help='GitHub repository to check (in format username/reponame)')
-@click.option('--sleep', help='Number of second before another check (Default: 60)')
-def run (repo, sleep):
-    try:
-        # simple parameter validation
-        # There are three sources - command line switch, config file and default value
-        # If program is invoked without any argument, use config values
-        # arguments from command line overrides config
+class IssueBot:
+   
+    def __init__ (self):
+        self.init_config_vals()
+        
+        self.session = requests.Session()
+        self.session.headers = {'Authorization': 'token ' + self.token, 'User-Agent': 'Python'}
+        
+    def init_config_vals (self):
         auth = configparser.ConfigParser()
         auth.read('auth.cfg')
 
-        if not repo:
-            repo = auth['github']['repo']
-
-        token = auth['github']['token']
+        self.repo = auth['github']['repo']
+        self.token = auth['github']['token']
 
         config = configparser.ConfigParser()
         config.read('settings.cfg')
 
-        if not sleep:
-            if 'sleep' in config['general']:
-                wait = int(config['general']['sleep'])
-            else:
-                wait = 60 # Default value if not set either in config or at runtime
+        if 'sleep' in config['general']:
+            self.wait = int(config['general']['sleep'])
         else:
-            wait = int(sleep)
+            self.wait = 60 # Default value if not set either in config or at runtime
 
-        # prepare session
+    def request (self, req):
+        print("Trying request: ", req)
+        r = self.session.get(req)
 
-        req_url = "https://api.github.com/repos/%s/issues" % repo
+        print("JSON response: ", json.dumps(r.json(), sort_keys=True, indent=4)) # pretty print
+        print("Response status code: ", r.status_code)
 
-        session = init_session(token)
-        
-        while (1):
-            reply = request(session, req_url)
-            print("Next check in %d seconds" % wait)
-            time.sleep(wait)
-    
-    except KeyboardInterrupt:
-        print('\n\nKeyboard exception received. Exiting.')
-    
-    finally:
-        session.close()
+        return r.json()
+
+#   @click.command()
+#   @click.option('--repo', help='GitHub repository to check (in format username/reponame)')
+#   @click.option('--sleep', help='Number of second before another check (Default: 60)')
+    def run (self, sleep=5):
+        try:
+            # simple parameter validation
+            # There are three sources - command line switch, config file and default value
+            # If program is invoked without any argument, use config values
+            # arguments from command line overrides config
+            
+            # prepare session
+            req_url = "https://api.github.com/repos/%s/issues" % self.repo
+            
+            while (1):
+                reply = self.request(req_url)
+                print("Next check in %d seconds" % self.wait)
+                time.sleep(self.wait)
+
+        except KeyboardInterrupt:
+            print('\n\nKeyboard exception received. Exiting.')
+
+        finally:
+            self.session.close()
 
 if __name__ == '__main__':
-    run()
+    robot = IssueBot()
+    robot.run()
