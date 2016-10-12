4ad30392a075f5a7dfef20055abd3127d66ca20c
Martin Matějek <matejm14@fit.cvut.cz>
2016-10-12 06:36:21 +0200

    Working click command line params

--- gh-issue-bot.py
+++ gh-issue-bot.py
@@ -10,27 +10,40 @@ import time
 class IssueBot:
    
     def __init__ (self):
-        self.init_config_vals()
+        self.session = None
+        self.repo = None
+        self.token = None
+        self.wait = None
+
+    def init (self, repo, sleep, auth, main, rules):
+        self.init_config_vals(repo, sleep, auth, main, rules)
         
         self.session = requests.Session()
         self.session.headers = {'Authorization': 'token ' + self.token, 'User-Agent': 'Python'}
         
-    def init_config_vals (self):
+    def init_config_vals (self, repo, sleep, authcfg, maincfg, rulescfg):
         auth = configparser.ConfigParser()
-        auth.read('auth.cfg')
+        auth.read(authcfg)
 
-        self.repo = auth['github']['repo']
+        if not repo:
+            self.repo = auth['github']['repo']
+        else:
+            self.repo = repo
+
         self.token = auth['github']['token']
 
         config = configparser.ConfigParser()
-        config.read('settings.cfg')
+        config.read(maincfg)
 
-        if 'sleep' in config['general']:
-            self.wait = int(config['general']['sleep'])
+        if not sleep:
+            if 'sleep' in config['general']:
+                self.wait = int(config['general']['sleep'])
+            else:
+                self.wait = 60 # Default value if not set either in config or at runtime
         else:
-            self.wait = 60 # Default value if not set either in config or at runtime
+            self.wait = int(sleep)
 
-    def request (self, req):
+    def get_issues (self, req):
         print("Trying request: ", req)
         r = self.session.get(req)
 
@@ -38,22 +51,16 @@ class IssueBot:
         print("Response status code: ", r.status_code)
 
         return r.json()
-
-#   @click.command()
-#   @click.option('--repo', help='GitHub repository to check (in format username/reponame)')
-#   @click.option('--sleep', help='Number of second before another check (Default: 60)')
-    def run (self, sleep=5):
+   
+    def start (self, repo, sleep, auth, config, rules):
         try:
-            # simple parameter validation
-            # There are three sources - command line switch, config file and default value
-            # If program is invoked without any argument, use config values
-            # arguments from command line overrides config
-            
             # prepare session
+            self.init(repo, sleep, auth, config, rules) 
+
             req_url = "https://api.github.com/repos/%s/issues" % self.repo
             
             while (1):
-                reply = self.request(req_url)
+                reply = self.get_issues(req_url)
                 print("Next check in %d seconds" % self.wait)
                 time.sleep(self.wait)
 
@@ -63,6 +70,16 @@ class IssueBot:
         finally:
             self.session.close()
 
-if __name__ == '__main__':
+@click.command()
+@click.option('--repo', help='GitHub repository to check (format username/reponame)')
+@click.option('--sleep', help='Number of second before another check')
+@click.option('--auth', default='auth.cfg', help='File with github credentials')
+@click.option('--config', default='settings.cfg', help='Main config file')
+@click.option('--rules', default='rules.cfg', help='Rules definition file')
+def run (repo, sleep, auth, config, rules):
+        
     robot = IssueBot()
-    robot.run()
+    robot.start(repo, sleep, auth, config, rules)
+
+if __name__ == '__main__':
+    run()
