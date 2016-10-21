1c18ca88f48e5ad012406d7ff20250f0f886b931
Martin Matejek <matejm14@fit.cvut.cz>
2016-10-21 02:38:27 +0200

    Set hook URL in config file

--- flaskapp.py
+++ flaskapp.py
@@ -1,17 +1,22 @@
 from flask import Flask, request, render_template
 from issuebot import IssueBot
+from configparser import ConfigParser
 
 webapp = Flask(__name__)
 robot = IssueBot()
- 
+
+config = ConfigParser()
+config.read('flask.cfg')
+
+
 @webapp.route('/')
 def index():
-    return render_template('index.html', title='Yet Another GitHub Issue Bot', description='github issue bot')
+    return render_template('index.html', title='Yet Another GitHub Issue Bot', description='github issue bot', hook_url=config['hook']['url'])
 
 @webapp.route('/hook', methods=['POST'])
 def hook ():
     data = request.get_json()
-    print("Json_data recieved: ", data)
+    #print("Json_data recieved: ", data)
 
     repo = data['repository']['full_name']
     
